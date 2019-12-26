23ddceebd11b7feb398c07e2532d98da950251b6
ch-ankit <akgunner6@gmail.com>
2019-12-25 20:24:27 -0800

    Abstract class

---
+++ BinaryTree.h
@@ -0,0 +1,11 @@
+#ifndef BinaryTree_h
+#define BinaryTree_h
+
+class BinarySearchTree{
+public:
+    virtual void preorderTraversal()=0;
+    virtual void add(int data)=0;
+    virtual bool search(int data)=0;
+};
+
+#endif /*BinaryTree_h */
