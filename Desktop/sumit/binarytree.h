0251ad500b0dd731a28b0f5d329e1ea638d5a055
Karan Rai <122karan.rai@gmail.com>
2019-12-26 09:58:57 +0545

    up

---
+++ Desktop/sumit/binarytree.h
@@ -0,0 +1,10 @@
+#ifindef BinaryTree_h
+#define BInaryTree_h
+#endif // BinaryTree_h
+
+class BInarySearchTree{
+public;
+    virtual void preordertraversal()=0;
+    virtual void add(int data)=0;
+    virtual bool search(int data)=0;
+};
