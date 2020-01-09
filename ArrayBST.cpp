52eeaca07c50754c955d14f40943bf519b6e04ad
sumitkhanal <55523893+sumitkhanal@users.noreply.github.com>
2020-01-09 22:23:02 +0545

    Add files via upload

--- ArrayBST.cpp
+++ ArrayBST.cpp
@@ -8,45 +8,73 @@ ArrayBST::ArrayBST(){
 }
 
 ArrayBST::~ArrayBST(){
-
 }
 
 void ArrayBST::add(int data){
-	if(this->element[0]==0){
-		element[1]=data;
-	}
-	else{
-		for(int i=1;i<=MAX_SIZE; ){
-			if(data<this->element[i]){
-				i=2*i;
-			}
-			else{
-				i=2*i+1;
-			}
+	//representing tree in array with first space empty
+	for(int i=1;i<=MAX_SIZE; ){
+		if(this->element[i]==0){
+			this->element[i]=data;
+			break;
+		}
+		else if(data<this->element[i]){
+			i=2*i;
+		}
+		else{
+			i=2*i+1;
+		}
+		if(i>MAX_SIZE){
+			std::cout<<"The array is full"<<std::endl;
+		}
+		else{
 			if(this->element[i]==0){
 				this->element[i]=data;
 				break;
 			}
+			else{
+				continue;
+			}
 		}
 	}
-	this->element[0]=this->element[0]+1;
 }
 
 void ArrayBST::preorderTraversal(){
-
+	
 }
 
 bool ArrayBST::search(int data){
-
+	for(int i=0;i<MAX_SIZE;){
+		if(data<this->element[i]){
+			i=2*i;
+		}
+		else{
+			i=2*i+1;
+		}
+		if(this->element[i]==data){
+			return true;
+			break;
+		}
+		else
+		{
+			continue;
+		}
+		
+	}
+	return false;
 }
 
 int main(){
 	ArrayBST a;
+	a.add(10);
 	a.add(5);
 	a.add(6);
 	a.add(8);
+	a.add(4);
+	a.add(3);
 
 	for(int i=0; i<MAX_SIZE;i++){
-		std::cout<<a.element[i];
+		std::cout<<a.element[i]<<"\t";
 	}
+	std::cout<<std::endl;
+	std::cout<<a.search(9)<<" "<<a.search(8);
 }
--- ArrayBST.h
+++ ArrayBST.h
@@ -1,15 +1,15 @@
 #include "BinaryTree.h"
 
-#define MAX_SIZE 16
+#define MAX_SIZE 12
 
 class ArrayBST: public BinarySearchTree{
 	//private:
 	
 	public:
+		int element[MAX_SIZE];
 		ArrayBST();
 		~ArrayBST();
 		void preorderTraversal();
 		void add(int data);
 		bool search(int data);
-		int element[MAX_SIZE];
 };
